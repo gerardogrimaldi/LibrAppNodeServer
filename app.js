e3d28d510e8b291e72c8b8d1803405964128e64a
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2014-03-22 00:23:17 -0300

    ver 22/3

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ var express     = require('express');
 var http        = require('http');
 var path 		= require('path');
 
+
 //mongoose.connect('mongodb://librapp:pass@server');
 var app = express();
 
+process.env.PWD = process.cwd();
+
 app.configure(function(){
   app.set('port', process.env.PORT || 8000);
-  app.set('views', __dirname + '/views');
+  //app.set('views', __dirname + '/views');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  //app.use(express.static(process.env.PWD + '/public'));
+  app.use(express.static(path.normalize(path.join(__dirname, '/public')), { maxAge: 86400000 }));
+  //app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
@@ -28,12 +33,14 @@ app.all('/*', function (req, res, next) {
   next();
 });
 
-app.use(app.router);
-app.use(function(req, res) {
-    res.set('Content-Type', 'text/html'); 
+/*app.use("/", function(req, res) {
+      res.setHeader('Content-Type', mimeType);
     // Use res.sendfile, as it streams instead of reading the file into memory.
     res.sendfile(__dirname + '/public/index.html');
+});*/
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port %s in %s mode.",  app.get('port'), app.settings.env);
 });
-var port = process.env.PORT || 8000;
-app.listen(port);
-console.log('Listening on port 80');
+
---
+++ public/scripts/app.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var app = angular.module('LibrApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ngGrid'
+  //,'ui.bootstrap'
+  ,'restangular'
+])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/',              {templateUrl: 'views/main.html',          controller: 'MainCtrl'})
+      .when('/navbar',        {templateUrl: 'views/navbar.html',        controller: 'MainCtrl'})
+      .when('/users',         {templateUrl: 'views/user/index.html',    controller: 'IndexUserCtrl'})
+      .when('/user/login',    {templateUrl: 'views/user/login.html',    controller: 'IndexUserCtrl'})
+      .when('/user/register', {templateUrl: 'views/user/create.html',   controller: 'IndexUserCtrl'})
+      //.when('/user/create', {templateUrl: 'views/user/create.html', controller: 'UserCreateCtrl'})
+      
+      .otherwise({redirectTo: '/404'});
+    //$locationProvider.html5Mode(true);
+  }]);
+
+app.config(['$httpProvider', function($httpProvider) {
+      //Enable cross domain calls
+      $httpProvider.defaults.useXDomain = true;
+
+      //Remove the header used to identify ajax call  that would prevent CORS from working
+      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
+app.config(function(RestangularProvider) {
+  //RestangularProvider.setBaseUrl('https://libraps-c9-gerardogrimaldi.c9.io/api');
+  RestangularProvider.setBaseUrl('http://api.gerardogrimaldi.com/api');
+
+  RestangularProvider.setDefaultHttpFields({cache: true});
+  //RestangularProvider.setMethodOverriders(["put", "patch"]);
+
+  // In this case we are mapping the id of each element to the _id field.
+  // We also change the Restangular route. 
+  // The default value for parentResource remains the same.
+  RestangularProvider.setRestangularFields({
+    id: "_id",
+    route: "restangularRoute",
+    selfLink: "self.href"
+  });
+});
+
+
+//Ejemplo de injeccion restangular 
+// Here it injects Restangular by itself
+//angular.module('angularjs-starter').controller('NewCtrl', function($scope, Restangular) {
+  // My controller code
+//});
